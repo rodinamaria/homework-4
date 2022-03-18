048db9f41ccead055f5819a81a8b68e779246714
mariarodina <q6m4?Qskk#>
2022-03-18 15:40:56 +0300

    домашняя работа 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,33 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Задача 1
+        int number = 0;
+        while (number < 10) {
+            System.out.print(++ number + " ");
+        }
+        System.out.println();
+        for (; number > 0; number --) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+
+// Задача 2
+        int firstFriday = 5;
+        for(int fridayReport = firstFriday; fridayReport <= 31; fridayReport += 7) {
+            System.out.println("Сегодня пятница " + fridayReport + " , необходимо подготовить отчет");
+        }
+
+
+// Задача 3
+        int now = 2022;
+        int start = now - 200;
+        int finish = now + 100;
+        for (int year = start; year <= finish; year++) {
+            if (year % 79 == 0) {
+                System.out.println(year);
+            }
+        }
+
     }
 }
